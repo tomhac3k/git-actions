9a90ded7e7554b125a658ce4dc6a165acf1c5497
tomhac3k <tomhac3k@gmail.com>
2024-12-27 21:08:31 +0530

    Mistake

--- elephantsay.sh
+++ elephantsay.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cowsay -f elephant -f "Simply put, things always had to be in a production-ready state: if you wrote it, you darn well had to be there to get it running!" > elephantsay.txt
+cowsay -f elephant 'Simply put, things always had to be in a production-ready state: if you wrote it, you darn well had to be there to get it running!' > elephantsay.txt
