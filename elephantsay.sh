8960fd460ef628bc7e17d9c1d29dbc796252f605
tomhac3k <tomhac3k@gmail.com>
2024-12-27 20:56:43 +0530

    Created a multi job CICD with variable

---
+++ elephantsay.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cowsay -f elephant -f "Simply put, things always had to be in a production-ready state: if you wrote it, you darn well had to be there to get it running!" > elephantsay.txt
